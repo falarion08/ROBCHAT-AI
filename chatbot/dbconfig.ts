d825e77f90b974cff539defb803148e99d4674de
falarion08 <barrion.ryanonil@gmail.com>
2025-01-29 15:40:07 +0800

    Update dbconfig.ts

--- chatbot/dbconfig.ts
+++ chatbot/dbconfig.ts
@@ -7,15 +7,14 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYaMyJQ_nc40uUHV68MiMr7bcr775Uq4c",
-  authDomain: "chatbot-ai-d4911.firebaseapp.com",
-  projectId: "chatbot-ai-d4911",
-  storageBucket: "chatbot-ai-d4911.firebasestorage.app",
-  messagingSenderId: "651546962485",
-  appId: "1:651546962485:web:125f5bd34a07c1f6ccdf7c",
-  measurementId: "G-JQNCWL2E5W"
+  apiKey: process.env.DB_KEY,
+  authDomain: process.env.DB_AUTHDOMAIN,
+  projectId: process.env.DB_PROJECT_ID,
+  storageBucket: process.env.DB_STORAGEBUCKET,
+  messagingSenderId: String(process.env.DB_MESSAGING_SENDER_ID),
+  appId: process.env.DB_APP_ID,
+  measurementId: process.env.DB_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
