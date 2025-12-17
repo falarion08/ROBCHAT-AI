6cfc60e17ba16024c14e825e0a55bdacd5134cf9
Ryan <113824865+falarion08@users.noreply.github.com>
2025-12-17 04:43:12 +0000

    set up project folder

--- chatbot/src/app/actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-import { cookies } from 'next/headers'
-import { deleteSession } from '@/app/lib/session'
-import { redirect } from 'next/navigation'
- 
-export async function logout() {
-  deleteSession()
-  redirect('/');
-}
