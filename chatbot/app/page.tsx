8b6f4982e44967dd75d7d0166f6b6f2f343522c4
falarion08 <ryanawsome101@gmail.com>
2025-12-17 06:22:56 +0000

    test2

--- chatbot/app/page.tsx
+++ chatbot/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <Button>Cick me</Button>
+      <Button>Cick medsfaf</Button>
     </div>
   )
 }
